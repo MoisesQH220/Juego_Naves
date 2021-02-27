e71a385e06e8a89ca259724a4c5d2efa9c74e7a9
Moisés Andrés Alejandro Quispe Herrera <mquispeh@everis.com>
2021-02-27 17:49:21 -0500

    Addded changes

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    public Vector3 direction;
+    public float speed;
+
+    private void Update()
+    {
+        transform.Translate(direction * speed * Time.deltaTime);
+    }
+    // Start is called before the first frame update
+    public void DoMove(Vector3 moverValue)
+    {
+        transform.Translate(moverValue);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Boundary
+{
+    public float xMinimum, xMaximum, yMinimum, yMaximum;
+}
+
+public class PlayerController : MonoBehaviour
+{
+    public Mover moverComponent;
+    public float speed;
+    public Boundary boundary;
+
+    public Transform shootOrigin;
+    public GameObject shootPrefab;
+
+    public Mover MoverComponent { get => moverComponent; set => moverComponent = value; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        moverComponent.speed = speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 desplazamiento = new Vector3(Input.GetAxis("Horizontal") * speed * Time.deltaTime,
+            Input.GetAxis("Vertical") * speed * Time.deltaTime, transform.position.z);
+        moverComponent.DoMove(desplazamiento);
+
+        float x = Mathf.Clamp(transform.position.x, boundary.xMinimum, boundary.xMaximum);
+        float y = Mathf.Clamp(transform.position.y, boundary.yMinimum, boundary.yMaximum);
+        transform.position = new Vector3(x, y);
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(shootPrefab, shootOrigin, false);
+        }
+    }
+}
